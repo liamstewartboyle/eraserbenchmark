e20219f58e2c16e4954825471d5b3a5f09366f95
Liam Boyle <liam.stewart.boyle@gmail.com>
2022-06-22 16:13:36 +0200

    setup push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eraserbenchmark',
+    version='',
+    packages=['rationale_benchmark', 'rationale_benchmark.models', 'rationale_benchmark.models.pipeline'],
+    url='',
+    license='',
+    author='xxx',
+    author_email='',
+    description=''
+)
